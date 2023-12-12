14ffa7016bb79920f387007a15dff583476e1d41
Allan Miller <millerallan17@gmail.com>
2023-12-12 09:12:52 -0300

    add: separei o historico da main

--- src/routes/historic_blueprint.py
+++ src/routes/historic_blueprint.py
@@ -12,3 +12,27 @@ def find_():
     response = make_response([historico.to_json() for historico in historicos])
     return response
 
+# listar historico escolar pelo id do aluno
+@historic_blueprint.route('/historico/<int:user_id>', methods=['GET'])
+def get_historic_by_id(user_id):
+    historicos = Historico.query.get(user_id)
+    if not historicos:
+        response = make_response({'message': 'Historico não encontrado'})
+        response.status_code = 404
+    else:
+        response = make_response([historico.to_json() for historico in historicos])
+        response.status_code = 200
+    return response
+
+@historic_blueprint.route('/historico/<int:id_aluno>/<int:id_disciplina>', methods=['GET'])
+def get_historic_by_ids(id_aluno, id_disciplina):
+    historicos = Historico.query.filter_by(id_aluno=id_aluno, id_disciplina=id_disciplina).all()
+
+    if not historicos:
+        response = make_response({'message': 'Histórico não encontrado'})
+        response.status_code = 404
+    else:
+        response = make_response([historico.to_json() for historico in historicos])
+        response.status_code = 200
+
+    return response
