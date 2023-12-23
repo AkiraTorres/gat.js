facf05adea0f08b490712c874ff35f3237dc33f0
AkiraTorres <torres.thg@gmail.com>
2023-12-23 05:29:52 -0300

    add: media de carga horaria por disciplina

--- src/routes/subject_blueprint.py
+++ src/routes/subject_blueprint.py
@@ -247,14 +247,31 @@ def get_students_failed_more_than_times(id_disciplina, times):
     return response
 
 
-@subject_blueprint.route("/disciplina/media_creditos", methods=["GET"])
+@subject_blueprint.route("/disciplina/media/creditos", methods=["GET"])
 def get_average_credits_by_subject():
     try:
         data = db.session.query(func.avg(Disciplina.credito).label('avg')).filter(Disciplina.credito != -999)
 
         average = data[0].avg
 
-        response_data = {"average": eval(f"{average:.2f}")}
+        response_data = {"average_credits": eval(f"{average:.2f}")}
+        response = make_response(response_data)
+    
+    except Exception as e:
+        response = make_response({"error": str(e)})
+        response.status_code = 500  # Internal Server Error
+
+    return response
+
+
+@subject_blueprint.route("/disciplina/media/carga_horaria", methods=["GET"])
+def get_average_workload_by_subject():
+    try:
+        data = db.session.query(func.avg(Disciplina.carga_horaria).label('avg'))
+
+        average = data[0].avg
+
+        response_data = {"average_workload": eval(f"{average:.2f}")}
         response = make_response(response_data)
     
     except Exception as e:
