3ff3b5b41be80635673bb8779d3e8527baa3d92b
gustavoo151 <josegustavooliveira121@gmail.com>
2024-02-28 21:18:40 -0300

    Autenticação com retorno do cookie para httponly

--- src/routes/login_blueprint.py
+++ src/routes/login_blueprint.py
@@ -28,3 +28,27 @@ def login_usuarios():
         }), 200
 
     return jsonify({'message': 'Usuário ou senha incorretos'}), 401
+
+
+# Retorno com cookie de sessão
+@login_blueprint.route('/login-cookie', methods=['POST'])
+def login_usuarios_cookie():
+    data = request.get_json()
+
+    username = data.get('username')
+    senha = data.get('senha')
+
+    user = usuarios.query.filter(usuarios.username == username).first()
+
+    if user and user.check_password(senha):
+        session_id = create_access_token(identity=username, expires_delta=timedelta(days=1))
+
+        response = make_response(jsonify({
+            'message': 'Login realizado com sucesso'
+        }), 200)
+        response.set_cookie('session_id', session_id, httponly=True)
+
+        return response
+
+    return jsonify({'message': 'Usuário ou senha incorretos'}), 401
+
