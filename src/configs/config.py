ad4963352f480ed9ce4ddd5f517892cd3541780a
AkiraTorres <torres.thg@gmail.com>
2024-03-10 15:19:33 -0300

    fix: setando aplicação para funcionar com o enviroment online
    Signed-off-by: AkiraTorres <torres.thg@gmail.com>

--- src/configs/config.py
+++ src/configs/config.py
@@ -10,11 +10,12 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
 DB_PORT = os.getenv("DB_PORT")
+DB_ENV = os.getenv("DB_ENV")
 
 class Config:
     DEBUG = False
     DEVELOPMENT = False
-    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@localhost:{DB_PORT}/{DB_NAME}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USER}:{DB_PASS}@{DB_END}:{DB_PORT}/{DB_NAME}"
 
 
 class ProductionConfig(Config):
